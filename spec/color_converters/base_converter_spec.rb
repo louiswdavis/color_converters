c6945abd328eae762210d2e8a2dda966617f70d6
Louis Davis <79977953+louiswdavis@users.noreply.github.com>
2025-08-20 17:28:19 +0100

    typo

--- spec/color_converters/base_converter_spec.rb
+++ spec/color_converters/base_converter_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 
 RSpec.describe ColorConverters::BaseConverter do
   context 'methods' do
-    it 'conversions' do
+    it 'conversions to colour spaces' do
       colour = ColorConverters::Color.new(r: 51, g: 102, b: 204, a: 0.8)
 
       expect(colour.alpha).to eq 0.8
@@ -12,45 +12,45 @@ RSpec.describe ColorConverters::BaseConverter do
       expect(colour.hex).to eq('#3366cc')
       expect(colour.hsl).to eq({ h: 220.0, s: 60, l: 50 })
       expect(colour.hsv).to eq({ h: 220.0, s: 75, v: 80 })
-      expect(colourr.hsb).to eq({ h: 220.0, s: 75, b: 80 })
-      expect(colourr.cmyk).to eq({ c: 75, m: 50, y: 0, k: 20 })
-      expect(colourr.xyz).to eq({ x: 17.01, y: 14.57, z: 59.04 })
-      expect(colourr.cielab).to eq({ l: 45.03, a: 18.72, b: -57.85 })
+      expect(colour.hsb).to eq({ h: 220.0, s: 75, b: 80 })
+      expect(colour.cmyk).to eq({ c: 75, m: 50, y: 0, k: 20 })
+      expect(colour.xyz).to eq({ x: 17.01, y: 14.57, z: 59.04 })
+      expect(colour.cielab).to eq({ l: 45.03, a: 18.72, b: -57.85 })
       expect(colour.cielch).to eq({ l: 45.03, c: 60.80, h: 287.93 })
-      expect(colourr.oklab).to eq({ l: 53.25, a: -0.02, b: -0.17 })
-      expect(colourr.oklch).to eq({ l: 53.25, c: 0.17, h: 262.29 })
+      expect(colour.oklab).to eq({ l: 53.25, a: -0.02, b: -0.17 })
+      expect(colour.oklch).to eq({ l: 53.25, c: 0.17, h: 262.29 })
 
-      colourr = ColorConverters::Color.new(r: 64, g: 104, b: 193)
-      expect(colourr.cmyk).to eq({ c: 66.84, m: 46.11, y: 0.0, k: 24.31 })
+      colour = ColorConverters::Color.new(r: 64, g: 104, b: 193)
+      expect(colour.cmyk).to eq({ c: 66.84, m: 46.11, y: 0.0, k: 24.31 })
 
-      colourr = ColorConverters::Color.new(r: 1, g: 1, b: 1)
-      expect(colourr.name).to eq nil
+      colour = ColorConverters::Color.new(r: 1, g: 1, b: 1)
+      expect(colour.name).to eq nil
 
       # xyz
-      colourr = ColorConverters::Color.new(r: 64, g: 104, b: 193)
+      colour = ColorConverters::Color.new(r: 64, g: 104, b: 193)
       expect(colour.xyz).to eq({ x: 16.69, y: 14.84, z: 52.44 })
 
       # cie
-      colourr = ColorConverters::Color.new(r: 64, g: 104, b: 193)
-      expect(colourr.xyz).to eq({ x: 16.69, y: 14.84, z: 52.44 })
+      colour = ColorConverters::Color.new(r: 64, g: 104, b: 193)
+      expect(colour.xyz).to eq({ x: 16.69, y: 14.84, z: 52.44 })
       expect(colour.cielab).to eq({ l: 45.41, a: 15.27, b: -50.87 })
       expect(colour.cielch).to eq({ l: 45.41, c: 53.11, h: 286.71 })
 
       colour = ColorConverters::Color.new(r: 140, g: 76, b: 201)
       expect(colour.cielab).to eq({ l: 45.58, a: 50.33, b: -54.90 })
-      expect(colourr.cielch).to eq({ l: 45.58, c: 74.48, h: 312.52 })
+      expect(colour.cielch).to eq({ l: 45.58, c: 74.48, h: 312.52 })
 
-      colourr = ColorConverters::Color.new(r: 255, g: 255, b: 255)
+      colour = ColorConverters::Color.new(r: 255, g: 255, b: 255)
       expect(colour.cielab).to eq({ l: 100.0, a: 0.0, b: 0.0 })
-      expect(colourr.cielch).to eq({ l: 100.0, c: 0.0, h: 0.0 })
+      expect(colour.cielch).to eq({ l: 100.0, c: 0.0, h: 0.0 })
 
-      colourr = ColorConverters::Color.new(r: 0, g: 0, b: 0)
-      expect(colourr.cielab).to eq({ l: 0.0, a: 0.0, b: 0.0 })
-      expect(colourr.cielch).to eq({ l: 0.0, c: 0.0, h: 0.0 })
+      colour = ColorConverters::Color.new(r: 0, g: 0, b: 0)
+      expect(colour.cielab).to eq({ l: 0.0, a: 0.0, b: 0.0 })
+      expect(colour.cielch).to eq({ l: 0.0, c: 0.0, h: 0.0 })
 
-      colourr = ColorConverters::Color.new(r: 255, g: 0, b: 197)
-      expect(colourrr.cielab).to eq({ l: 57.36, a: 90.93, b: -32.79 })
-      expect(colourr.cielch).to eq({ l: 57.36, c: 96.66, h: 340.17 })
+      colour = ColorConverters::Color.new(r: 255, g: 0, b: 197)
+      expect(colour.cielab).to eq({ l: 57.36, a: 90.93, b: -32.79 })
+      expect(colour.cielch).to eq({ l: 57.36, c: 96.66, h: 340.17 })
 
       # testing the episilon for LCH conversion fallback
       colour = ColorConverters::Color.new(h: 255, s: 0, l: 50)
@@ -59,7 +59,7 @@ RSpec.describe ColorConverters::BaseConverter do
 
       colour = ColorConverters::Color.new(h: 255, s: 1, l: 50)
       expect(colour.cielab).to eq({ l: 53.01, a: 0.75, b: -1.3 })
-      expect(colour.cielch).to eq({ l: 53.01, c: 1.5, h: 299.92 })
+      expect(colour.cielch).to eq({ l: 53.01, c: 1.5, h: 300.03 })
 
       # ok
       colour = ColorConverters::Color.new(r: 64, g: 104, b: 193)
@@ -77,15 +77,15 @@ RSpec.describe ColorConverters::BaseConverter do
 
       colour = ColorConverters::Color.new(r: 0, g: 0, b: 0)
       expect(colour.oklab).to eq({ l: 0.0, a: 0.0, b: 0.0 })
-      expect(colourr.oklch).to eq({ l: 0.0, c: 0.0, h: 0.0 })
+      expect(colour.oklch).to eq({ l: 0.0, c: 0.0, h: 0.0 })
 
-      colourr = ColorConverters::Color.new(r: 255, g: 0, b: 197)
-      expect(colourr.oklab).to eq({ l: 67.12, a: 0.27, b: -0.09 })
+      colour = ColorConverters::Color.new(r: 255, g: 0, b: 197)
+      expect(colour.oklab).to eq({ l: 67.12, a: 0.27, b: -0.09 })
       expect(colour.oklch).to eq({ l: 67.12, c: 0.29, h: 342.2 })
     end
 
     it 'conversions (white point)' do
-      colourr = ColorConverters::Color.new(r: 255, g: 255, b: 255, a: 0.8)
+      colour = ColorConverters::Color.new(r: 255, g: 255, b: 255, a: 0.8)
 
       expect(colour.alpha).to eq 0.8
       expect(colour.name).to eq 'white'
